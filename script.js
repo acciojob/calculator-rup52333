1b4ce8e924b39e0a8cd3d0af541f32ab821c4764
Rupkumar Sarkar <109466063+rup52333@users.noreply.github.com>
2023-02-05 23:21:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your code here
+
+(function() {
+	let input=document.querySelector("#input");
+let buttons=document.querySelector(".calculator__keys");
+let clear=document.querySelector("#clr");
+let equal=document.querySelector("#ans");
+
+buttons.forEach(function(button){
+button.addEventListener('click',function(e){
+let value=e.target.dataset.num;
+input.value+=value;
+});
+    
+
+});
+
+
+	
+})();
+
+
+
+
